ae0fa04c295f17d82cd210b4eb26b146c0a54f90
Erwin Handoko <bhasunjaya@gmail.com>
2012-03-20 00:11:15 +0700

    n

--- sql.sql
+++ sql.sql
@@ -1,23 +1,7 @@
-  SELECT
-            count(ap.id) as total_pax,
-            DAY(voucher_created_at) as hari
-        FROM aero_passengers ap
-            LEFT JOIN aero_vouchers av ON av.id=ap.voucher_id
-            LEFT JOIN aero_airlines aa ON aa.id=av.airlines_id
-        WHERE 
-            ap.id = ? 
-            AND YEAR(voucher_created_at) = ?
-            AND MONTH(voucher_created_at) = ?
-        GROUP BY hari
-        ORDER BY hari ASC
 
-
-SELECT
-*
-        FROM aero_passengers ap
-            LEFT JOIN aero_vouchers av ON av.id=ap.voucher_id
-            LEFT JOIN aero_airlines aa ON aa.id=av.airlines_id
-        WHERE 
-            ap.id = 2 
-            AND YEAR(voucher_created_at) = 2012
-            AND MONTH(voucher_created_at) = 2
+SELECT ap.voucher_type, COUNT( ap.id ) AS total
+            FROM  `aero_passengers` ap
+                LEFT JOIN aero_vouchers av ON av.id = ap.voucher_id
+WHERE 
+    av.voucher_created_at >= DATE_ADD(CURDATE(), INTERVAL -3 DAY);
+GROUP BY ap.voucher_type
