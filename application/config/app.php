2ae16768a92e6a6acea211e5f1cb183c71d45287
Erwin Handoko <bhasunjaya@gmail.com>
2012-04-17 01:52:56 +0700

    reordering document

--- application/config/app.php
+++ application/config/app.php
@@ -7,10 +7,10 @@ $config['app_username'] = array(
 );
 
 
-$config['doc_upload_type'][0] = 'kronologis';
 $config['doc_upload_type'][1] = 'telex';
 $config['doc_upload_type'][2] = 'manifest';
 $config['doc_upload_type'][3] = 'movement';
+$config['doc_upload_type'][0] = 'kronologis';
 $config['doc_upload_type'][4] = 'tanda bukti';
 $config['doc_upload_type'][5] = 'others';
 
