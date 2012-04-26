db60d51983f5940b69450359622cffea8ae4f739
Erwin Handoko <bhasunjaya@gmail.com>
2012-04-26 12:00:20 +0700

    garuda exception

--- application/models/passenger_model.php
+++ application/models/passenger_model.php
@@ -27,6 +27,30 @@ class Passenger_Model extends CI_Model {
         $res = $this->db->query($sql, array($voucher_id))->result();
         return $res;
     }
+    
+    
+    function get_passengers_garuda($voucher_id, $mode='all') {
+    	$print_sql = '';
+        if ($mode == 'print') {
+            //$print_sql = " AND passenger_name <> '' AND passenger_ticket <> ''";
+        } else {
+            $print_sql = '';
+        }
+        $sql = "
+            SELECT
+            *,ap.id,au.fullname
+            FROM aero_passengers ap
+                LEFT JOIN aero_vouchers av ON av.id=ap.voucher_id
+                LEFT JOIN aero_users au ON au.id=av.user_id
+                
+            WHERE ap.voucher_id=? 
+            $print_sql 
+            ORDER BY av.voucher_created_at DESC
+            
+            ";
+        $res = $this->db->query($sql, array($voucher_id))->result();
+        return $res;
+    }
 
 }
 
